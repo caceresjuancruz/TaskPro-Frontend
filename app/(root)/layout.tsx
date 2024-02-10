6a97494c6a6451af4a9164b8e4522efa237322a6
Juan Cruz Cáceres <106564947+caceresjuancruz@users.noreply.github.com>
2024-02-10 02:57:00 -0300

    Add user avatar and logout button to root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,39 @@
+import { truncateString } from '@/lib/utils'
 import { LayoutProps } from '@/types'
+import { Button } from '@nextui-org/button'
+import { User } from '@nextui-org/user'
+import Image from 'next/image'
 
 export default async function RootLayout({ children }: LayoutProps) {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='mb-6 border-b-2 border-gray-50 pb-2 text-3xl font-bold'>
-          TaskPro🎯
-        </h1>
+        <div className='mb-6 flex items-center justify-between border-b-2 border-gray-50 pb-2'>
+          <h1 className='text-3xl font-bold'>TaskPro🎯</h1>
+          <div className='flex gap-2'>
+            <User
+              name='User'
+              description={truncateString('caceresjuan.dev@gmail.com')}
+              avatarProps={{
+                src: '/assets/images/userplaceholder.jpg'
+              }}
+            />
+            <Button
+              isIconOnly
+              color='default'
+              variant='light'
+              aria-label='Logout'
+            >
+              <Image
+                src={'/assets/icons/logout.svg'}
+                unoptimized
+                alt='Logout'
+                height={20}
+                width={20}
+              />
+            </Button>
+          </div>
+        </div>
         {children}
       </div>
     </section>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,9 @@
 //Utility functions for the app
+
+export const truncateString = (str: string): string => {
+  if (str.length > 20) {
+    return str.substring(0, 15) + '...'
+  } else {
+    return str
+  }
+}
